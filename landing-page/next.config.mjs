f97810eed6582bc7c8be1d10ec432257b74e5862
Akolawole <olaboyekolawole420@gmail.com>
2025-03-22 15:26:59 +0100

    Server

--- landing-page/next.config.mjs
+++ landing-page/next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+import { createServer } from "http";
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const server = createServer((req, res) => {
+  res.end("Hello World");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
 
 const nextConfig = {};
