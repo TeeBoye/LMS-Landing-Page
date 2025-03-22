4cc53097169c62b5f1fab36b19b25fa4c5d7c0fe
Akolawole <olaboyekolawole420@gmail.com>
2025-03-22 15:34:31 +0100

    Instances fixed

--- landing-page/next.config.mjs
+++ landing-page/next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
-import { createServer } from "http";
+import http from "http";
 
-const server = createServer((req, res) => {
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone", // Let Render handle the port
+};
+
+const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
 
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
-const nextConfig = {};
 
 export default nextConfig;
