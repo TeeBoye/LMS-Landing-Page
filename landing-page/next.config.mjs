f6b1464ca0f805c5427b01243f4431260ab08928
Akolawole <olaboyekolawole420@gmail.com>
2025-03-22 15:18:54 +0100

    Port Setup

--- landing-page/next.config.mjs
+++ landing-page/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 const nextConfig = {};
 
 export default nextConfig;
