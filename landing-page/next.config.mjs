a19901948257cf8920a1c07fce8a2faacdd13881
Akolawole <olaboyekolawole420@gmail.com>
2025-03-22 15:39:39 +0100

    Package Update

--- landing-page/next.config.mjs
+++ landing-page/next.config.mjs
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
-import http from "http";
-
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone", // Let Render handle the port
 };
-
-const server = http.createServer((req, res) => {
-  res.end("Hello World");
-});
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
 
 export default nextConfig;
